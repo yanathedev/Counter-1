0bac85c199ff2dc38cabb7fbbfa84d439a2b8cfe
yanathedev <kochubynska.design@gmail.com>
2023-03-07 17:09:35 +0100

    Added styles

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
+// let count = 0;
+// const btns = document.querySelectorAll('.btn');
+// const value = document.querySelector('#value');
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function(e) {
+//         const styles = e.currentTarget.classList;
+//         if (styles.contains('decrease')) {
+//             count--;
+//         }
+//         else if (styles.contains('increase')) {
+//             count++;
+//         }
+//         else {
+//             count = 0;
+//         }
+//         if (count > 0){
+//             value.style.color = 'green';
+//         }
+//         else if (count < 0){
+//             value.style.color = 'red';
+//         }
+//         else if (count === 0){
+//             value.style.color = 'black';
+//         }
+
+//         value.textContent = count;
+//     })
+// })
+
 let count = 0;
 const btns = document.querySelectorAll('.btn');
 const value = document.querySelector('#value');
 
-btns.forEach(function (btn) {
+btns.forEach( function (btn){
     btn.addEventListener('click', function(e) {
         const styles = e.currentTarget.classList;
-        if (styles.contains('decrease')) {
+        if (styles.contains('decrease')){
             count--;
         }
-        else if (styles.contains('increase')) {
-            count++;
+        else if (styles.contains('increase')){
+            count++
         }
-        else {
+        else{
             count = 0;
         }
         if (count > 0){
-            value.style.color = 'green';
+            value.style.color = '#617C8C';
         }
         else if (count < 0){
-            value.style.color = 'red';
+            value.style.color = '#617C8C';
         }
         else if (count === 0){
-            value.style.color = 'black';
+            value.style.color = '#F2F2F2';
         }
 
-        value.textContent = count;
+        value.textContent= count;
     })
 })
+    
